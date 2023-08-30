0a4e4dd4dcfdafa47ceab921a10450d13bde1de8
ryosuke-horie <ryosuke03140@gmial.com>
2023-08-31 02:21:22 +0900

    :bug: 遷移先URLを変更

--- src/middleware.ts
+++ src/middleware.ts
@@ -24,7 +24,7 @@ export async function middleware(request: NextRequest) {
 
   // 取得したIPアドレスがホワイトリストに含まれているかチェックし、含まれていない場合はアクセス拒否
   if(!IP_WHITELIST.includes(ip)){
-    return NextResponse.redirect('https://middleware-ip-limit.vercel.app/access-denied'); // アクセス拒否のページにリダイレクト
+    return NextResponse.redirect('https://middleware-ip-limit-one.vercel.app/access-denied'); // アクセス拒否のページにリダイレクト(フルパスで指定)
   }
 
   return res;
